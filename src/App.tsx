1540ea68b5d755ed68498f58eec47af0fecbf5cd
Asante Laryea Akrong <kal79@byu.edu>
2024-03-09 19:33:15 -0700

    More Fundamentals

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import movieData from './MovieData.json';
 
+const mds = movieData.MovieData;
+
+function MovieList() {
+  const [listOMovies, setListOMovies] = useState(mds);
+
+  const addMovie = () => {
+    setListOMovies([
+      ...mds,
+      {
+        Category: 'Action',
+        Director: 'Christopher Nolan',
+        Rating: 'PG-13',
+        Title: 'Inception',
+        Year: '2010',
+      },
+      {
+        Category: 'Action',
+        Director: 'Christopher Nolan',
+        Rating: 'PG-13',
+        Title: 'Interstellar',
+        Year: '2014',
+      },
+    ]);
+  };
+  return (
+    <>
+      <h2> This is the movie colleciton list</h2>
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Year</th>
+            <th>Director</th>
+            <th>Rating</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listOMovies.map((m) => (
+            <tr>
+              <td>{m.Title}</td>
+              <td>{m.Year}</td>
+              <td>{m.Director}</td>
+              <td>{m.Rating}</td>
+              <td>{m.Category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <br />
+      <button onClick={addMovie}>Add Movie</button>
+    </>
+  );
+}
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  function refreshClock() {
+    setTime(new Date());
+  }
+  useEffect(() => {
+    const timerid = setInterval(refreshClock, 1000);
+    return () => clearInterval(timerid);
+  }, []);
+  return (
+    <div>
+      <pre>{new Date().toLocaleTimeString()}</pre>
+    </div>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieList />
+      <Clock />
     </div>
   );
 }
